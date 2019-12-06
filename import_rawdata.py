a941ac755f1a053bd06572bddd19e39c85d7e510
YongTaekLee <llyt3@naver.com>
2019-12-06 20:27:41 +0900

    파일제목 띄어쓰기 수정
    수정

--- import_rawdata.py
+++ import_rawdata.py
@@ -1,29 +1,29 @@
-#%% import
-%reset -f
-import os
+#%%_import
+%reset_-f
+import_os
 os.getcwd()
-from os import chdir
+from_os_import_chdir
 
-# import sys
-import pandas as pd
-import seaborn as sns # 시각화
-import matplotlib.pyplot as plt
-# 그래프의 스타일을 지정
+#_import_sys
+import_pandas_as_pd
+import_seaborn_as_sns_#_시각화
+import_matplotlib.pyplot_as_plt
+#_그래프의_스타일을_지정
 plt.style.use('ggplot')
-import numpy as np
-import scipy as sp
-import sklearn
-import matplotlib as mpl
+import_numpy_as_np
+import_scipy_as_sp
+import_sklearn
+import_matplotlib_as_mpl
 mpl.rcParams.update({'font.size':14})
-plt.rc('font',family='Malgun Gothic') # windows
-%matplotlib inline 
+plt.rc('font',family='Malgun_Gothic')_#_windows
+%matplotlib_inline
 
-pd.set_option('display.expand_frame_repr', False) # expand output display pd.df
+pd.set_option('display.expand_frame_repr',_False)_#_expand_output_display_pd.df
 #%%
-data1 = pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회 L.POINT Big Data Competition-분석용데이터-01.온라인 행동 정보.csv",low_memory=False) 
-data2 = pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회 L.POINT Big Data Competition-분석용데이터-02.거래 정보.csv") 
-data3 = pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회 L.POINT Big Data Competition-분석용데이터-03.고객 Demographic 정보.csv") 
-data4 = pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회 L.POINT Big Data Competition-분석용데이터-04.상품분류 정보.csv") 
+data1_=_pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회_L.POINT_Big_Data_Competition-분석용데이터-01.온라인_행동_정보.csv",low_memory=False)_
+data2_=_pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회_L.POINT_Big_Data_Competition-분석용데이터-02.거래_정보.csv")_
+data3_=_pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회_L.POINT_Big_Data_Competition-분석용데이터-03.고객_Demographic_정보.csv")_
+data4_=_pd.read_csv("C:\\Users\\MYCOM\\Dropbox\\LFU\\제6회_L.POINT_Big_Data_Competition-분석용데이터-04.상품분류_정보.csv")_
 
 
 
