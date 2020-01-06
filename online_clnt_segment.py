5670182c4bef936e896cfdfdc07b3564ab8567e9
12jeong <haniijoeng827@gmail.com>
2020-01-07 01:53:34 +0900

    update

--- online_clnt_segment.py
+++ online_clnt_segment.py
@@ -13,7 +13,7 @@ import sklearn
 import matplotlib as mpl
 
 # -- DIRECTORY
-os.chdir('C:\\Users\\MYCOM\\Dropbox')
+os.chdir('C:/Users/UOS/Dropbox')
 pdir = os.getcwd() ;print(pdir)
 
 # -- PLOT STYLE
@@ -25,10 +25,10 @@ plt.rc('font',family='Malgun Gothic') # windows
 pd.set_option('display.expand_frame_repr', False) 
 
 #%% Load data
-df_buy  = pd.read_csv(".\LFY\\datasets/ppdata\\df_buy.csv") 
-online_bh  = pd.read_csv(".\LFY\\datasets\\ppdata\\online_bh.csv" )
-trans_info = pd.read_csv(".\LFY\\datasets/ppdata\\trans_info.csv") 
-# df_nobuy = pd.read_csv("./LFY\\datasets/ppdata\\df_nobuy.csv") 
+df_buy  = pd.read_csv("./LFY/datasets/ppdata/df_buy.csv") 
+online_bh  = pd.read_csv("./LFY/datasets/ppdata/online_bh.csv" )
+trans_info = pd.read_csv("./LFY/datasets/ppdata/trans_info.csv") 
+# df_nobuy = pd.read_csv("./LFY/datasets/ppdata/df_nobuy.csv") 
 
 online_bh['sess_dt'] =  pd.to_datetime(online_bh['sess_dt'])
 trans_info['de_dt'] =  pd.to_datetime(trans_info['de_dt'])
@@ -66,9 +66,15 @@ clnt_interval_tmp['days_gt10'][clnt_interval_tmp['days_diff'] > 10] = 1
 clnt_interval_tmp2 = clnt_interval_tmp.groupby(['biz_unit','clnt_id']).agg({'days_le3':'sum'}).reset_index()
 clnt_interval_tmp3 = clnt_interval_tmp.groupby(['biz_unit','clnt_id']).agg({'days_le10':'sum'}).reset_index()
 clnt_interval_tmp4 = clnt_interval_tmp.groupby(['biz_unit','clnt_id']).agg({'days_gt10':'sum'}).reset_index()
-clnt_interval = clnt_interval_tmp[['clnt_id','biz_unit']].merge(clnt_interval_tmp2, how="inner").merge(clnt_interval_tmp3, how="inner").merge(clnt_interval_tmp4, how="inner")
+clnt_interval = clnt_interval_tmp.drop_duplicates(['clnt_id','biz_unit'])[['clnt_id','biz_unit']].merge(clnt_interval_tmp2, how="inner").merge(clnt_interval_tmp3, how="inner").merge(clnt_interval_tmp4, how="inner")
 # clnt_interval_tmp['days_diff'].median()
 # clnt_interval_tmp['days_diff'].describe()
+#clnt_interval_tmp[clnt_interval_tmp.biz_unit=="A01"]['days_diff'].median()
+#clnt_interval_tmp[clnt_interval_tmp.biz_unit=="A01"]['days_diff'].describe()
+#clnt_interval_tmp[clnt_interval_tmp.biz_unit=="A02"]['days_diff'].median()
+#clnt_interval_tmp[clnt_interval_tmp.biz_unit=="A02"]['days_diff'].describe()
+#clnt_interval_tmp[clnt_interval_tmp.biz_unit=="A03"]['days_diff'].median()
+#clnt_interval_tmp[clnt_interval_tmp.biz_unit=="A03"]['days_diff'].describe()
 # np.quantile(clnt_interval_tmp[~pd.isnull(clnt_interval_tmp.days_diff)].days_diff, 0.5)
 # np.quantile(clnt_interval_tmp[~pd.isnull(clnt_interval_tmp.days_diff)].days_diff, 0.8)
 
@@ -97,6 +103,13 @@ buy_interval = pd.merge(buy_interval_tmp2, buy_interval_tmp3, how="inner").merge
 
 # buy_interval_tmp['buy_diff'].median()
 # buy_interval_tmp['buy_diff'].describe()
+
+#buy_interval_tmp[buy_interval_tmp.biz_unit=="A01"]['buy_diff'].median()
+#buy_interval_tmp[buy_interval_tmp.biz_unit=="A01"]['buy_diff'].describe()
+#buy_interval_tmp[buy_interval_tmp.biz_unit=="A02"]['buy_diff'].median()
+#buy_interval_tmp[buy_interval_tmp.biz_unit=="A02"]['buy_diff'].describe()
+#buy_interval_tmp[buy_interval_tmp.biz_unit=="A03"]['buy_diff'].median()
+#buy_interval_tmp[buy_interval_tmp.biz_unit=="A03"]['buy_diff'].describe()
 # np.quantile(buy_interval_tmp[~pd.isnull(buy_interval_tmp.buy_diff)].buy_diff, 0.5)
 # np.quantile(buy_interval_tmp[~pd.isnull(buy_interval_tmp.buy_diff)].buy_diff, 0.8)
 
@@ -118,78 +131,91 @@ df1['hit_diff_ratio'] = df1['hit_diff']/ df1['tot_sess_hr_v'] # 시간의 비율
 # -- 세션 내에서 검색한 unique한 검색어 개수
 df_kwd_tmp = df1[~pd.isnull(df1.sech_kwd)].groupby(['biz_unit','clnt_id','sess_id','sess_dt'])['sech_kwd'].apply(lambda x: len(np.unique(list(x)))).reset_index()
 df_kwd = df_kwd_tmp.groupby(['biz_unit','clnt_id'])['sech_kwd'].agg('mean').reset_index()
+df_kwd  = df_kwd.fillna(0)
+
 # -- 세션 내에서 가장 관심이 오래 있었던 제품의 세부정보 보기 시간
 df_max_2time_tmp = df1[df1.action_type==2].groupby(['biz_unit','clnt_id','sess_id','sess_dt'])['hit_diff'].agg({'max_2time':'max'})
 df_max_2time = df_max_2time_tmp.groupby(['biz_unit','clnt_id'])['max_2time'].agg('mean').reset_index()
+df_max_2time = df_max_2time.fillna(0)
+
 # -- 첫 구매까지 걸린 시각 
 df_buytime_tmp = df1[df1.action_type==6].sort_values(['biz_unit','clnt_id','sess_id','sess_dt','hit_seq']
                                                  ).groupby(['biz_unit','clnt_id','sess_id','sess_dt'])['hit_pss_tm'].nth(0).reset_index()   
 df_buytime = df_buytime_tmp.groupby(['biz_unit','clnt_id'])['hit_pss_tm'].agg('mean').reset_index()
 df_buytime.columns = ['biz_unit','clnt_id','buy_time']
-# 각 행동 수행 빈도 (freq)
-total_hit_seq = df1.sort_values(['biz_unit','clnt_id','sess_id','sess_dt','hit_seq']).groupby(['biz_unit','clnt_id','sess_id','sess_dt']).nth(-1)['hit_seq']
-count_hit_seq_tmp = df1.groupby(['biz_unit','clnt_id','sess_id','sess_dt','action_type'])['hit_seq'].agg('count')
-freq_hit_seq_tmp = count_hit_seq_tmp/total_hit_seq
-df2 = freq_hit_seq_tmp.unstack(level=-1, fill_value=0).reset_index()   # count accorinding to action_type
-df2.rename(columns = {0 : 'action_count_0', 1 : 'action_count_1', 2 : 'action_count_2', 3 : 'action_count_3',
-                      4 : 'action_count_4', 5 : 'action_count_5', 6 : 'action_count_6', 7 : 'action_count_7'}, inplace = True)
-df_action_freq = df2.groupby(['biz_unit','clnt_id']).agg('mean').reset_index().drop('sess_id',axis=1)
 
+# -- 장바구니 담는 횟수
+action3_count_tmp = df1[df1.action_type==3].groupby(['biz_unit','clnt_id','sess_id','sess_dt'])['action_type'].agg({'action3_count':'count'}).reset_index()
+action3_count_tmp = action3_count_tmp.drop(['sess_id','sess_dt'],axis=1)
+action3_count = action3_count_tmp.groupby(['biz_unit','clnt_id']).agg('mean').reset_index()
+
+# -- 각 행동 수행 빈도 (freq)
+#total_hit_seq = df1.sort_values(['biz_unit','clnt_id','sess_id','sess_dt','hit_seq']).groupby(['biz_unit','clnt_id','sess_id','sess_dt']).nth(-1)['hit_seq']
+#count_hit_seq_tmp = df1.groupby(['biz_unit','clnt_id','sess_id','sess_dt','action_type'])['hit_seq'].agg('count')
+#freq_hit_seq_tmp = count_hit_seq_tmp/total_hit_seq
+#df2 = freq_hit_seq_tmp.unstack(level=-1, fill_value= np.nan ).reset_index()   # count accorinding to action_type
+#df2.rename(columns = {0 : 'action_count_0', 1 : 'action_count_1', 2 : 'action_count_2', 3 : 'action_count_3',
+#                      4 : 'action_count_4', 5 : 'action_count_5', 6 : 'action_count_6', 7 : 'action_count_7'}, inplace = True)
+#df2 = df2.drop(['sess_id','sess_dt'],axis=1)
+#df_action_freq = df2.groupby(['biz_unit','clnt_id']).agg(np.nanmean).reset_index()
+#df_action_freq = df_action_freq.fillna(0)
 # -- 각 행동 소요시간 비율 (ratio)
-df3 = df1.groupby(['biz_unit','clnt_id','sess_id','sess_dt','action_type'])['hit_diff_ratio'].agg('sum').unstack(level=-1, fill_value=0).reset_index()       # time consuming by action_type
-df3.rename(columns = {0 : 'action_time_0', 1 : 'action_time_1', 2 : 'action_time_2', 3 : 'action_time_3',
-                      4 : 'action_time_4', 5 : 'action_time_5', 6 : 'action_time_6', 7 : 'action_time_7'}, inplace = True)
-df_action_time = df3.groupby(['biz_unit','clnt_id']).agg('mean').reset_index().drop('sess_id',axis=1)
+#df3 = df1.groupby(['biz_unit','clnt_id','sess_id','sess_dt','action_type'])['hit_diff_ratio'].agg('sum').unstack(level=-1, fill_value=np.nan).reset_index()       # time consuming by action_type
+#df3.rename(columns = {0 : 'action_time_0', 1 : 'action_time_1', 2 : 'action_time_2', 3 : 'action_time_3',
+#                      4 : 'action_time_4', 5 : 'action_time_5', 6 : 'action_time_6', 7 : 'action_time_7'}, inplace = True)
+#df3 = df3.drop(['sess_id','sess_dt'],axis=1)
+#df_action_time = df3.groupby(['biz_unit','clnt_id']).agg(np.nanmean).reset_index()
+#df_action_time = df_action_time.fillna(0)
+
 
 # -- merge for clnt_info
-clnt_info = clnt_info_tmp.merge(df_kwd,how="left").merge(df_max_2time,how="left").merge(df_buytime,how="left").merge(df_action_freq,how="left").merge(df_action_time,how="left")
-clnt_info.columns
-clnt_info = clnt_info.drop(['clnt_gender','action_count_4','action_count_5','action_count_6','action_count_7',
-                            'action_time_4','action_time_5','action_time_6','action_time_7'],axis=1)
+clnt_info = clnt_info_tmp.merge(df_kwd,how="left").merge(df_max_2time,how="left").merge(df_buytime,how="left").merge(action3_count,how="left")
+clnt_info['action3_count'] = clnt_info.action3_count.fillna(0)
+clnt_info['max_2time'] = clnt_info.max_2time.fillna(0)
+clnt_info['sech_kwd'] = clnt_info.sech_kwd.fillna(0)
+#clnt_info = clnt_info_tmp.merge(df_kwd,how="left").merge(df_max_2time,how="left").merge(df_buytime,how="left").merge(df_action_freq,how="left").merge(df_action_time,how="left")
+#clnt_info.columns
+clnt_info = clnt_info.drop('clnt_gender',axis=1)
+#clnt_info = clnt_info.drop(['clnt_gender','action_count_4','action_count_5','action_count_6','action_count_7',
+#                            'action_time_4','action_time_5','action_time_6','action_time_7'],axis=1)
 clnt_info['dvc_ctg_nm'][clnt_info.dvc_ctg_nm=='mobile_app'] = 'mobile'
 clnt_info['dvc_ctg_nm'][clnt_info.dvc_ctg_nm=='mobile_web'] = 'mobile'
 clnt_info.columns
+
 #%% final table for customer information
-clnt_info.to_csv("./LFY/datasets/ppdata/clnt_info.csv",index=False)
+#clnt_info.to_csv("./LFY/datasets/ppdata/clnt_info.csv",index=False)
 
 #%% K-means
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-
-clnt_info = pd.read_csv("./LFY/datasets/ppdata/clnt_info.csv")
+# clnt_info = pd.read_csv("./LFY/datasets/ppdata/clnt_info.csv")
 # only for buy client
 clnt_buy_info = clnt_info[~pd.isnull(clnt_info.buy_num)]
 clnt_buy_A01 = clnt_buy_info [clnt_buy_info.biz_unit == "A01"]
 clnt_buy_A02 = clnt_buy_info [clnt_buy_info.biz_unit == "A02"]
 clnt_buy_A03 = clnt_buy_info [clnt_buy_info.biz_unit == "A03"]
 # continuos variable for kmeans
-X = clnt_buy_A01.drop(['biz_unit','clnt_id','trfc_src','dvc_ctg_nm','clnt_gender','clnt_age','member'],axis=1)
+X = clnt_buy_A03.drop(['biz_unit','clnt_id','trfc_src','dvc_ctg_nm','clnt_age','member','max_2time','action3_count'],axis=1)
+X_col = X.columns
+X = X.dropna()
 # In general, it's a good idea to scale the data 
 scaler = StandardScaler()
 scaler.fit(X)
 X=scaler.transform(X)    
 
 # kmeans
-kmeans = KMeans(n_clusters=10)
+kmeans = KMeans(n_clusters=4)
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
 
-# plot PCA loading and loading in biplot
-y = y_kmeans
-def myplot(score,coeff,labels=None):
-    xs = score[:,0]
-    ys = score[:,1]
-    n = coeff.shape[0]
-    scalex = 1.0/(xs.max() - xs.min())
-    scaley = 1.0/(ys.max() - ys.min())
-    plt.scatter(xs * scalex,ys * scaley, c = y)
-    plt.xlim(-1,1)
-    plt.ylim(-1,1)
-    plt.xlabel("PC{}".format(1))
-    plt.ylabel("PC{}".format(2))
-    plt.grid()
+X_df = pd.DataFrame(X)
+X_df.columns = X_col
+X_df['y'] = y_kmeans
 
-myplot(x_new[:,0:2],np.transpose(pca.components_[0:2, :]))
-plt.show()
+for i in range(len(X_col)):
+   fig, ax = plt.subplots(figsize=(7,5))
+   plt.suptitle('')
+   X_df.boxplot(column=X_col[i], by='y', ax=ax)
+   plt.show()
